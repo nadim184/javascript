e5c75c94a6958c48636c5c34a0a55668ef0b85ff
nadim184 <51932411+nadim184@users.noreply.github.com>
2019-07-01 10:15:28 +0100

    Add files via upload

---
+++ jas.js
@@ -0,0 +1,16 @@
+function change(){
+  document.getElementById('txt').style.fontWeight="bold";
+}
+function cha(){
+  document.getElementById('txt').style.fontStyle="italic";
+}
+function chan(){
+  document.getElementById('txt').style.textDecoration="underline";
+}
+function changeSize(){
+    document.getElementById('txt').style.fontSize=document.getElementById('size').value;
+}
+function changestyle(){
+  document.getElementById('txt').style.fontFamily=document.getElementById('font').value;
+ 
+}
